bd9179372b7604548e8314eba7e24436e68ae465
Jhetal0001 <jhetal00@gmail.com>
2023-09-15 21:04:29 -0500

    project is created in spring boot

---
+++ backend/app_web_register/src/main/java/com/app_register/app/AppWebRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.app_register.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebRegisterApplication.class, args);
+	}
+
+}
